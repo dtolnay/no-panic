2b57b3a9bcb4c08df58959c2fb537f2745bd870a
David Tolnay <dtolnay@gmail.com>
2024-11-11 23:52:03 -0800

    Check rustc version before emitting cargo:rustc-check-cfg build instructions

--- build.rs
+++ build.rs
@@ -1,4 +1,27 @@
+use std::env;
+use std::process::Command;
+use std::str;
+
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-check-cfg=cfg(exhaustive)");
+
+    let rustc = match rustc_minor_version() {
+        Some(rustc) => rustc,
+        None => return,
+    };
+
+    if rustc >= 80 {
+        println!("cargo:rustc-check-cfg=cfg(exhaustive)");
+    }
+}
+
+fn rustc_minor_version() -> Option<u32> {
+    let rustc = env::var_os("RUSTC").unwrap();
+    let output = Command::new(rustc).arg("--version").output().ok()?;
+    let version = str::from_utf8(&output.stdout).ok()?;
+    let mut pieces = version.split('.');
+    if pieces.next() != Some("rustc 1") {
+        return None;
+    }
+    pieces.next()?.parse().ok()
 }
