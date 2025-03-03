a3a47fb13871d291b20c8598e5a82f7bc6bc666b
David Tolnay <dtolnay@gmail.com>
2025-03-03 15:47:05 -0800

    Release 0.1.34

--- src/lib.rs
+++ src/lib.rs
@@ -132,7 +132,7 @@
 //! [Kixunil]: https://github.com/Kixunil
 //! [`dont_panic`]: https://github.com/Kixunil/dont_panic
 
-#![doc(html_root_url = "https://docs.rs/no-panic/0.1.33")]
+#![doc(html_root_url = "https://docs.rs/no-panic/0.1.34")]
 #![allow(
     clippy::doc_markdown,
     clippy::match_same_arms,
